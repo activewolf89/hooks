23443348f6f97fdb1778544895afe64dde175210
Reed <Reeddenny@gmail.com>
2019-01-30 11:47:02 -0800

    super past!?

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const simpleGit = require('simple-git')();
 const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('The SUPER FUTURE WILL HAPPEN will happen\n');
+  res.end('The SUPER PAST?!?! WILL HAPPEN will happen\n');
 });
 app.get('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('The SUPER FUTURE WILL HAPPEN will happen\n');
+  res.end('The SUPER PAST?!?! WILL HAPPEN will happen\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

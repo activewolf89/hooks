f3fc23e57e1dccfeb987efa9b4b9d769ab1bb4e6
Reed <Reeddenny@gmail.com>
2019-01-30 15:47:49 -0800

    FUCK

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const simpleGit = require('simple-git')();
 const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('Blahhh zzz\n');
+  res.end('FUCK\n');
 });
 app.post('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('Blahhh zzz\n');
+  res.end('FUCK\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

b341b34dd957bfda01bc6a4c48d50b98d83b4ad4
Reed <Reeddenny@gmail.com>
2019-01-30 12:02:14 -0800

    pretty close

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const simpleGit = require('simple-git')();
 const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('i think we ready\n');
+  res.end('i think we are pretty close to ready\n');
 });
 app.get('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('i think we ready\n');
+  res.end('i think we are pretty close to ready\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

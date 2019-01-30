edfc009fce366061081989a9cacd9706e4e21831
Reed <Reeddenny@gmail.com>
2019-01-30 12:05:59 -0800

    NOBLOOD FOR ME

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const simpleGit = require('simple-git')();
 const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('i think we are pretty close to ready\n');
+  res.end('THERE WILL NOT BE BLOOD\n');
 });
 app.post('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('i think we are pretty close to ready\n');
+  res.end('THERE WILL NOT BE BLOOD\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

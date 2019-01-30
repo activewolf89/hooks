0a1515232f3d8dbac6593f90f23376fbf7bf5275
Reed <Reeddenny@gmail.com>
2019-01-29 19:13:36 -0800

    second update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ http.createServer(function (request, response) {
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World this was an update!\n');
 }).listen(8081);
 
 // Console will print the message

e25c972500452e80c480672c4efcf2dc45dcae9d
Reed <Reeddenny@gmail.com>
2019-01-30 11:34:40 -0800

    super future

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var path = require('path');
 var app = express();
 var bodyParser = require('body-parser')
 const simpleGit = require('simple-git')();
-const port = 8080
+const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('The Past will happen\n');
+  res.end('The SUPER FUTURE WILL HAPPEN will happen\n');
 });
 app.get('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('The Past will happen\n');
+  res.end('The SUPER FUTURE WILL HAPPEN will happen\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

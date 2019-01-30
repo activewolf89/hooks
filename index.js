af9cf295d3bb811357c9b9078c792165a4277038
Reed <Reeddenny@gmail.com>
2019-01-30 15:23:22 -0800

    HAHA

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const simpleGit = require('simple-git')();
 const port = 8081
 
 app.get('/', function(req, res) {
-  res.end('HI NICOLE :D THE INTERNET NOW KNOWS I LOVE YOUUUUU\n');
+  res.end('HI NICOLE :D THE INTERNET NOW KNOWS YOU WET THE BEDDDDD\n');
 });
 app.post('/updateWebsite', function(req, res) {
   console.log('update the website')
-  res.end('HI NICOLE :D THE INTERNET NOW KNOWS I LOVE YOUUUUU\n');
+  res.end('HI NICOLE :D THE INTERNET NOW KNOWS YOU WET THE BEDDDDD\n');
   simpleGit.pull((err,test)=>{
     console.log(err)
     console.log(test)

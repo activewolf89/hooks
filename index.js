d43ee7770ce46a561f869279adc37d485e25545e
Reed <Reeddenny@gmail.com>
2019-01-29 21:05:49 -0800

    fuck

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('The future may be brighter than one thinks\n');
+   response.end('-_-0\n');
 }).listen(8081);
 
 // Console will print the message
